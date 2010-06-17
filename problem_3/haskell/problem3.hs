b1927c6dd2095fdd7150743f4cfe4d2c69673166
Bryce Verdier <bryceverdier@gmail.com>
2010-06-16 22:49:59 -0700

    added prime function. It works but is too slow.

--- problem_3/haskell/problem3.hs
+++ problem_3/haskell/problem3.hs
@@ -3,4 +3,12 @@ zero_mod divisor
   | otherwise = False
   where divide = mod 600851475143 divisor
 
+prime :: Integer -> Integer -> Bool
+prime divisor divided
+  | divisor== divided= True
+  | mod divided divisor== 0 = False
+  | otherwise = next_prime
+  where next_prime = prime (divisor + 2) divided
 
+is_prime :: Integer -> Bool
+is_prime input = prime 3 input
