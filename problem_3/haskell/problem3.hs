d86af65640fffcdc8d42c0fbe035cce51c175b7c
Bryce Verdier <bryceverdier@gmail.com>
2010-06-17 00:03:30 -0700

    added divided = 1 = True to stop a near infinite loop

--- problem_3/haskell/problem3.hs
+++ problem_3/haskell/problem3.hs
@@ -1,10 +1,13 @@
-zero_mod divisor 
-  | divide == 0 = True
+import Data.List
+
+zero_mod divisor
+  | divide == 0 = True 
   | otherwise = False
   where divide = mod 600851475143 divisor
 
 prime :: Integer -> Integer -> Bool
 prime divisor divided
+  | divided == 1 = True
   | divisor== divided= True
   | mod divided divisor== 0 = False
   | otherwise = next_prime
