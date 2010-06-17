997e28e2fc2c25b7af4ffc4f0168926599908508
Bryce Verdier <bryceverdier@gmail.com>
2010-06-16 23:22:40 -0700

    initial setup for problem 5 in python

---
+++ problem_5/python/problem5.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+def div_1_to_20(divided):
+  divisor = 20
+  while( divisor >= 11):
+    if( divided % divisor != 0):
+      return False
+  
+  return True
+
+
+def main():
+  count = 2520
+  while div_1_to_20(count) == False:
+    count += 2520
+
+  print "%s" % count
+
+
+if __name__ == "__main__":
+  main()
+
