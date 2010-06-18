1c5c8ca0d0708891171783acca30bc87ef8fdbcd
Bryce Verdier <bryceverdier@gmail.com>
2010-06-17 19:39:03 -0700

    fixed some other one off errors, but have solution for problem 4 in python

--- problem_4/python/problem4.py
+++ problem_4/python/problem4.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python
 
 def is_palindrome(number):
-  half = int(.5 * number)
+  local_list = int_to_list(number)
+  half = int(.5 * len(local_list))
   count = 1
 
   while count <= half:
-    if number[count - 1] != number[-1 * count]:
+    if local_list[count - 1] != local_list[-1 * count]:
       return False
 
     count += 1
 
   return True
 
+def int_to_list(number):
+  local_list = []
+  while number:
+    digit = number % 10
+    local_list.append(digit)
+    number /= 10
+
+  return local_list
+
 
 def main():
   first_list = range(100,1000)
   first_list.reverse()
 
-  count1 = 1
-  count2= 1
+  count1 = 0
+  count2= 0
   flag = False
 
   while flag == False:
@@ -28,7 +38,7 @@ def main():
       flag = True
     else:
       count1 += 1
-      if count1 >= 999:
+      if count1 >= 900:
         count2 += 1
         count1 = count2 
 
