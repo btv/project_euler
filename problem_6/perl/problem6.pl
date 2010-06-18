e5ac12b020e8fa50a421d560341efddf20c4cd96
Bryce Verdier <bryceverdier@gmail.com>
2010-06-17 23:57:22 -0700

    problem 6 in perl; complete! Used some of the tricks from the blog to simply coding.

--- problem_6/perl/problem6.pl
+++ problem_6/perl/problem6.pl
@@ -1,3 +1,14 @@
 #!/usr/bin/perl
 
-use List::Util qw(sum)
+use List::Util qw(sum);
+
+my $sum1;
+my $sum2;
+
+for $n (1..100)
+{
+  $sum1 += $n;
+  $sum2 += $n * $n;
+}
+
+print ($sum1 * $sum1 - $sum2);
