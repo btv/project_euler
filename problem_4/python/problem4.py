624f68e8355bb804db319bbea4dcbd370fadd2c4
Bryce Verdier <bryceverdier@gmail.com>
2010-06-17 19:19:52 -0700

    need to figure out how to put the value of the product into a list

--- problem_4/python/problem4.py
+++ problem_4/python/problem4.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 def is_palindrome(number):
-  half = int(.5 * len(number))
+  half = int(.5 * number)
   count = 1
 
   while count <= half:
@@ -11,3 +11,27 @@ def is_palindrome(number):
     count += 1
 
   return True
+
+
+def main():
+  first_list = range(100,1000)
+  first_list.reverse()
+
+  count1 = 1
+  count2= 1
+  flag = False
+
+  while flag == False:
+    product = int(first_list[count1] * first_list[count2])
+    if is_palindrome(product):
+      print "%s" % (product)
+      flag = True
+    else:
+      count1 += 1
+      if count1 >= 999:
+        count2 += 1
+        count1 = count2 
+
+
+if __name__ == "__main__":
+  main()
