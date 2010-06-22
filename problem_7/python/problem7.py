7570b4ca5b58cbfdbaa8379274742bdf47dcb8cf
Bryce Verdier <bryceverdier@gmail.com>
2010-06-21 23:21:05 -0700

    problem 7 initial upload & final.

---
+++ problem_7/python/problem7.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import math
+
+# code taken from problem 3
+def is_prime(divided):
+  divisor = 3
+  sqrt_divided = math.sqrt(divided)
+  if divided == 1:
+    return True
+  else:
+    while divisor <= sqrt_divided:
+      if divided == divisor:
+        return True
+      elif divided % divisor == 0:
+        return False
+      else:
+        divisor += 2
+    return True
+
+
+def main():
+  prime_list = [2]
+  count = 3
+
+  # 2 is the first prime, since we're starting at 1, reducing the search
+  # by 1.
+  while len(prime_list) < 10000:
+    if is_prime(count) == True:
+      prime_list.append(count)
+
+    count += 2
+
+  print prime_list[-1]
+
+
+if __name__ == "__main__":
+  main()
