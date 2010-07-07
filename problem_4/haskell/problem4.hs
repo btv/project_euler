904278249244891423be67d1e684e3b195406ee1
Bryce Verdier <bryceverdier@gmail.com>
2010-07-06 22:42:49 -0700

    inital setup for problem 4 in Haskell. This one is going to be lots of fun.

---
+++ problem_4/haskell/problem4.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Data.List
+
+--Stole numberToList code from:
+--http://www.rhinocerus.net/forum/lang-functional/95473-converting-number-list-haskell.html
+--Need to learn about Haskell in order to learn how it works.
+numberToList = reverse . unfoldr (\x -> if x == 0 then Nothing else let (a,b) = x `quotRem` 10 in Just (b,a))
+
+numberToList' 0 = []
+numberToList' number = reverse . unfoldr (mod number 10 : numberToList' (quot number 10)) 
+
+
+--main :: IO ()
