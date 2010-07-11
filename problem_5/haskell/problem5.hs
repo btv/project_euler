e0492108680ac021bf1484e43384ddab934b9ea3
Bryce Verdier <bryceverdier@gmail.com>
2010-07-10 17:30:30 -0700

    changed haskell to use unfolr instead of pattern matching. In Python, removed a defined main function, just put it into the bottom if check.

--- problem_5/haskell/problem5.hs
+++ problem_5/haskell/problem5.hs
@@ -1,17 +1,13 @@
-divisible_1_to_20 n
-  | mod n 20 /= 0 = False
-  | mod n 19 /= 0 = False
-  | mod n 18 /= 0 = False
-  | mod n 17 /= 0 = False
-  | mod n 16 /= 0 = False
-  | mod n 15 /= 0 = False
-  | mod n 14 /= 0 = False
-  | mod n 13 /= 0 = False
-  | mod n 12 /= 0 = False
-  | mod n 11 /= 0 = False
-  | otherwise = True
+module Main where
+
+import Data.List
+
+divisible_1_to_20 number = 10 == (length $ unfoldr(\x -> if (snd $ quotRem number x) /= 0 || x < 11
+				                        then Nothing
+			                                else Just(x,x - 1)) 20)
 
 -- solved this by the help on this URL:
 -- http://basildoncoder.com/blog/2008/06/10/project-euler-problem-5/
 -- by increaing the loop from 2 to 2520, problem solves in seconds
-problem5 = until (divisible_1_to_20) (+2520) 2520
+main :: IO ()
+main = print $ until (divisible_1_to_20) (+2520) 2520
--- problem_5/python/problem5.py
+++ problem_5/python/problem5.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-def div_1_to_20(divided):
+def div_11_to_20(divided):
   divisor = 20
   while( divisor >= 11):
     if( divided % divisor != 0):
@@ -11,14 +11,10 @@ def div_1_to_20(divided):
   return True
 
 
-def main():
+if __name__ == "__main__":
   count = 2520
-  while div_1_to_20(count) == False:
+  while div_11_to_20(count) == False:
     count += 2520
 
   print "%s" % count
-
-
-if __name__ == "__main__":
-  main()
 
