21888bf781c24cf879acc0667c54eeb9785359e1
Bryce Verdier <bryceverdier@gmail.com>
2010-07-17 13:35:13 -0700

    modified python solution to use list comprehension instead of the iterative process I had before.

--- problem_5/python/problem5.py
+++ problem_5/python/problem5.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python
 
 def div_11_to_20(divided):
-  divisor = 20
-  while( divisor >= 11):
-    if( divided % divisor != 0):
-      return False
-
-    divisor -= 1
-  
-  return True
-
+  return all([not bool(divided % x) for x in xrange(11,20+1)])
 
 if __name__ == "__main__":
   count = 2520
