4acd9ce52acbd81985168c1049a174a0fe4b5727
Bryce Verdier <bryceverdier@gmail.com>
2010-07-28 16:33:34 -0700

    fixed formatting

--- problem_5/haskell/problem5.hs
+++ problem_5/haskell/problem5.hs
@@ -3,8 +3,8 @@ module Main where
 import Data.List
 
 divisible_1_to_20 number = 10 == (length $ unfoldr(\x -> if (snd $ quotRem number x) /= 0 || x < 11
-				                        then Nothing
-			                                else Just(x,x - 1)) 20)
+				                                                 then Nothing
+			                                                   else Just(x,x - 1)) 20)
 
 -- http://basildoncoder.com/blog/2008/06/10/project-euler-problem-5/
 -- by increaing the loop from 2 to 2520, problem solves in seconds
