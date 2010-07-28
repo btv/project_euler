e9f224c3bd3bd58a411583afd5e8426d5f0e07bd
Bryce Verdier <bryceverdier@gmail.com>
2010-07-27 22:38:29 -0700

    reduced unnessary code.

--- problem_1/java/problem1.java
+++ problem_1/java/problem1.java
@@ -4,8 +4,6 @@ public class problem1
 {
     public static void main( String[] args)
     {
-            //LinkedList ll = new LinkedList();
-
             for( int i = 1; i < 1000; i++)
             {
                 if( i % 3 == 0 || i % 5 == 0)
