18efeb3f5eaa656c89928b347b687bb9ff1169e2
Bryce Verdier <bryceverdier@gmail.com>
2010-07-27 22:47:59 -0700

    finished java solution for problem 1.

--- problem_1/java/problem1.java
+++ problem_1/java/problem1.java
@@ -4,12 +4,15 @@ public class problem1
 {
     public static void main( String[] args)
     {
+            int total = 0;
             for( int i = 1; i < 1000; i++)
             {
                 if( i % 3 == 0 || i % 5 == 0)
                 {
-                    System.out.println(i);
+                    total += i;
                 }
             }
+
+            System.out.println(total);
     }
 }
