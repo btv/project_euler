a1e9a686936f377f77659873a99ab7089a62ff48
Bryce Verdier <bryceverdier@gmail.com>
2010-07-28 17:02:42 -0700

    fixed wrong function title

--- problem_5/haskell/problem5.hs
+++ problem_5/haskell/problem5.hs
@@ -2,11 +2,11 @@ module Main where
 
 import Data.List
 
-divisible_1_to_20 number = 10 == (length $ unfoldr(\x -> if (snd $ quotRem number x) /= 0 || x < 11
+divisible_11_to_20 number = 10 == (length $ unfoldr(\x -> if (snd $ quotRem number x) /= 0 || x < 11
                                                          then Nothing
                                                          else Just(x,x - 1)) 20)
 
 -- http://basildoncoder.com/blog/2008/06/10/project-euler-problem-5/
 -- by increaing the loop from 2 to 2520, problem solves in seconds
 main :: IO ()
-main = print $ until (divisible_1_to_20) (+2520) 2520
+main = print $ until (divisible_11_to_20) (+2520) 2520
