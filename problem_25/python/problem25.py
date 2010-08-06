b063e85db5b2356893c6079d6fafc04d6c031a54
Bryce Verdier <bryceverdier@gmail.com>
2010-08-06 16:08:59 -0700

    solution for problem 25 in Python (Python3 compatible)

---
+++ problem_25/python/problem25.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+def fibinaci_generator():
+  flip = True
+  out = False
+  num1 = 1
+  num2 = 1
+  while True:
+    if flip == True:
+      num1 += num2
+      if len(str(num1)) == 4:
+        return num1
+      flip = False
+    else:
+      num2 += num1
+      if len(str(num2)) == 4:
+        return num2
+      flip = True
+
+if __name__ == "__main__":
+  print(fibinaci_generator())
