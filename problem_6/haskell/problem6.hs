aa32d3f0ab40999941b5dee89486d40e6a1dffb3
Bryce Verdier <bryceverdier@gmail.com>
2010-08-10 14:25:18 -0700

    changed variable names in haskell solution.

--- problem_6/haskell/problem6.hs
+++ problem_6/haskell/problem6.hs
@@ -3,6 +3,6 @@ module Main where
 main :: IO ()
 main = do
        let x = [1..100]
-       let sumy = sum $ map (^2) x
-       let sumx = (sum x) ^2
-       print (sumx - sumy)
+       let sum2 = sum $ map (^2) x
+       let sum1 = (sum x) ^2
+       print (sum1 - sum2)
