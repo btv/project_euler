1701055e720abfc875f29207d385c6b086d8d3e2
Bryce Verdier <bryceverdier@gmail.com>
2010-08-10 19:13:05 -0700

    Problem 6 java solution added.

---
+++ problem_6/java/problem6.java
@@ -0,0 +1,16 @@
+public class problem6
+{
+    public static void main( String[] args)
+    {
+      int sum1 = 0;
+      int sum2 = 0;
+      
+      for( int i = 0; i < 100; i++)
+      {
+        sum1 += i;
+        sum2 += i * i;
+      }
+
+      System.out.println(sum1 * sum1 - sum2);
+    }
+}
