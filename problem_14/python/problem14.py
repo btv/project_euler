7ffc4238c639e1da8061e2500996113779704f16
Bryce Verdier <bryceverdier@gmail.com>
2010-08-14 12:38:45 -0700

    Problem 14 framework is completed in Python. Might be to slow.

--- problem_14/python/problem14.py
+++ problem_14/python/problem14.py
@@ -4,28 +4,16 @@ def sequence(number):
   tmp_list = [number]
   
   while( number != 1):
-    if even(number):
-      number = number / 2
+    if number % 2 == 0:
+      number /= 2
     else:
       number = (number * 3) + 1
 
     tmp_list.append(number)
 
-  return tmp_list
-
-def even(number):
-  if ( number % 2 == 0):
-    return True
-  else:
-    return False 
-
-def main():
-  first_list = map(sequence, range(1,1000000+1))
-  second_list = zip( map(len, first_list), first_list)
-
-  print max(second_list)[1]
-
+  return (len(tmp_list), tmp_list)
 
 
 if __name__ == "__main__":
-  main()
+  print(max([sequence(x) for x in range(5,100000+1)]))
+
