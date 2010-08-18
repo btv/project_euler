65f04b42b478324ada9e3dce08ee16884c42882e
Bryce Verdier <bryceverdier@gmail.com>
2010-08-18 01:15:13 -0700

    stack overflow error on haskell. Answer recieved in Python, but it was too slow.

--- problem_14/haskell/problem14.hs
+++ problem_14/haskell/problem14.hs
@@ -4,11 +4,12 @@ import Data.List
 
 --sequence' :: (Num a) => a -> [a]
 sequence' a
-  | a == 1 = []
+  | a == 1 = [1]
   | even a == True = a: sequence' (round . fromIntegral $ div a 2)
   | otherwise = a: sequence' (3 * a + 1)
 
 main :: IO ()
 main = do
-    let seqx = map sequence' [5..1000000]
-    print $ seqx
+    let seqx = map sequence' [3,5..1000000]
+    let lengthx = map length seqx
+    print . maximum $ zip lengthx seqx
