730545f526ab63cea91080f26d7247f2084d9713
Bryce Verdier <bryceverdier@gmail.com>
2010-08-19 14:01:07 -0700

    python3 code is faster with directy squares instead of duplicate variable multiplication.

--- problem_6/python/problem6.py
+++ problem_6/python/problem6.py
@@ -4,9 +4,9 @@
 sum1 = 0
 sum2 = 0
 
-for i in ((x,x * x) for x in range(1,100+1)):
+for i in ((x,x ** 2) for x in range(1,100+1)):
     sum1 += i[0]
     sum2 += i[-1]
 
-print(sum1 * sum1 - sum2)
+print(sum1 ** 2 - sum2)
 
