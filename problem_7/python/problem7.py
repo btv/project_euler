c680a0f77011bb5e1921d2f5d4ba363f8127d42e
Bryce Verdier <bryceverdier@gmail.com>
2010-09-13 13:39:33 -0700

    removed unnessacary if statement.

--- problem_7/python/problem7.py
+++ problem_7/python/problem7.py
@@ -6,24 +6,19 @@ import math
 def is_prime(divided):
   divisor = 3
   sqrt_divided = math.sqrt(divided)
-  if divided == 1:
-    return True
-  else:
-    while divisor <= sqrt_divided:
-      if divided == divisor:
-        return True
-      elif divided % divisor == 0:
-        return False
-      else:
-        divisor += 2
-    return True
+  while divisor <= sqrt_divided:
+    if divided == divisor:
+      return True
+    elif divided % divisor == 0:
+      return False
+    else:
+      divisor += 2
+  return True
 
 if __name__ == "__main__":
   prime_list = [2]
   count = 3
 
-  # 2 is the first prime, since we're starting at 1, reducing the search
-  # by 1.
   while len(prime_list) < 10000:
     if is_prime(count) == True:
       prime_list.append(count)
