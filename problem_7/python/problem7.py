0f4c8b505656b6fa72e1ea1873b6051f3deec844
Bryce Verdier <bryceverdier@gmail.com>
2010-09-13 18:24:01 -0700

    haskell solution complete, fixed some errors with python code.

--- problem_7/python/problem7.py
+++ problem_7/python/problem7.py
@@ -2,24 +2,21 @@
 
 import math
 
-# code taken from problem 3
 def is_prime(divided):
   divisor = 3
   sqrt_divided = math.sqrt(divided)
   while divisor <= sqrt_divided:
-    if divided == divisor:
-      return True
-    elif divided % divisor == 0:
+    if divided % divisor == 0:
       return False
-    else:
-      divisor += 2
+
+    divisor += 2
   return True
 
 if __name__ == "__main__":
   prime_list = [2]
   count = 3
 
-  while len(prime_list) < 10000:
+  while len(prime_list) < 10001:
     if is_prime(count) == True:
       prime_list.append(count)
 
