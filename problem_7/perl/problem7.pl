0a886a45afa069871b65dd47dfc1a1955965c0db
Bryce Verdier <bryceverdier@gmail.com>
2010-09-13 20:39:52 -0700

    problem 7 solution in perl completed.

---
+++ problem_7/perl/problem7.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+sub is_prime
+{
+  my ($divided) = @_;
+  my $divisor = 3;
+  my $sqrt_divided = sqrt($divided);
+
+  while($divisor <= $sqrt_divided)
+  {
+    return 0 unless ($divided % $divisor);
+    $divisor += 2;
+  }
+
+  1;
+}
+
+
+my $count = 1;
+my $last;
+
+for ( my $i = 3; $count < 10001; $i += 2)
+{
+  if (is_prime($i))
+  {
+    $count += 1;
+    $last = $i;
+  }
+}
+
+print $last;
