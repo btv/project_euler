6804d8fd8abc718aa496a048081bb571c3e5e250
Bryce Verdier <bryceverdier@gmail.com>
2010-09-15 09:03:59 -0700

    added a version of python that used same algo as java & perl

---
+++ problem_7/python/problem7_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import math
+
+def is_prime(divided):
+  divisor = 3
+  sqrt_divided = math.sqrt(divided)
+  while divisor <= sqrt_divided:
+    if divided % divisor == 0:
+      return False
+
+    divisor += 2
+  return True
+
+
+if __name__ == "__main__":
+  count = 1 #to substitute the prime number 2 in the example
+  last = 0
+  iterator = 3
+
+  while count < 10001:
+    if is_prime(iterator):
+      count += 1
+      last = iterator
+
+    iterator += 2
+
+  print(last)
