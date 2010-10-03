4b595809d333c270342f7e358a5ea86c2c3b7123
Bryce Verdier <bryceverdier@gmail.com>
2010-10-02 18:44:20 -0700

    added a new solution, that uses a list for variable storage. Should be faster.

---
+++ problem_7/python/problem7_3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import math
+
+def is_prime(divided):
+  divisor = 3
+  sqrt_divided = math.sqrt(divided)
+  while divisor <= sqrt_divided:
+    if divided % divisor == 0:
+      return False
+
+    divisor += 2
+  return True
+
+
+if __name__ == "__main__":
+  # using a list to store each int value 
+  data = [1,0,3]
+
+  while data[0] < 10001:
+    if is_prime(data[2]):
+      data[0] += 1
+      data[1] = data[2]
+
+    data[2] += 2
+
+  print(data[1])
