512879d5901f5e99261dd297af33b6b7ae0037a4
Bryce Verdier <bryceverdier@gmail.com>
2010-10-05 10:00:06 -0700

    played with when sqrt is being called. Now the program is MUCH faster.

--- problem_7/haskell/problem7.hs
+++ problem_7/haskell/problem7.hs
@@ -1,12 +1,13 @@
 module Main where
 
-prime divisor divided
+prime_wrapper divisor divided = prime divisor divided (round . sqrt. fromIntegral $ divided)
+
+prime divisor divided sqrt_divided
   | divisor > sqrt_divided = [divided]
   | mod divided divisor == 0 = []
   | otherwise = next_prime
-  where next_prime = prime (divisor + 2) divided
-        sqrt_divided = round . sqrt . fromIntegral $ divided
+  where next_prime = prime (divisor + 2) divided sqrt_divided
 
 main :: IO ()
 main = print $ primes !! 10000
-  where primes = 2 : [ x | x <- [3,5..],  prime 3 x /= [] ]
+  where primes = 2 : [ x | x <- [3,5..],  prime_wrapper 3 x /= [] ]
