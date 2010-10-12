b021a4cda70604e492e34dc21cb4caa17cd1ced6
Bryce Verdier <bryceverdier@gmail.com>
2010-10-12 10:08:49 -0700

    trying to get problem 9 working, got incorrect result.

--- problem_9/python/problem9.py
+++ problem_9/python/problem9.py
@@ -15,14 +15,14 @@ def pythagorianTriple(a,b):
   """
   if the square root of a^2 + b^2 mod'ed = 0, we have something.
   """
-  temp_sqrt = math.sqrt(a * a + b * b)
-  if int(temp_sqrt % 1) == 0:
-    return (True, int(temp_sqrt))
+  temp_sqrt = int(math.sqrt(a * a + b * b))
+  if temp_sqrt % 1 == 0:
+    return (True, temp_sqrt)
   else:
     return (False, 0)
 
 
-def main():
+if __name__ == "__main__":
   count_left = 1
   count_right = 1
 
@@ -41,8 +41,3 @@ def main():
     else:
       count_left += 1
       flip = flip ^ 1
-
-
-
-if __name__ == "__main__":
-  main()
