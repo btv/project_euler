e331917ba65064baec8f58cec1accc9471b4d550
Bryce Verdier <bryceverdier@gmail.com>
2010-10-12 17:56:34 -0700

    modified the code so that main was one line.

--- problem_9/python/problem9.py
+++ problem_9/python/problem9.py
@@ -3,7 +3,6 @@
 python soltion for project euler problem #9.
 """
 
-x = [a*b*(1000 - b -a) for a in range(1,500+1) for b in range(1,500+1) \
-        if a * a + b * b == ((1000 -b -a) ** 2)]
+print( [a*b*(1000 - b -a) for a in range(1,500+1) for b in range(1,500+1) \
+        if a * a + b * b == ((1000 -b -a) ** 2)][0])
 
-print x[0]
