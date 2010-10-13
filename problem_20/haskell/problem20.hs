0dde8cda01de11207f3bd56d4ac1e22a868e5ee5
Bryce Verdier <bryceverdier@gmail.com>
2010-10-12 17:55:39 -0700

    needed to do the same for haskell solution.

--- problem_20/haskell/problem20.hs
+++ problem_20/haskell/problem20.hs
@@ -1,7 +1,14 @@
 module Main where
 -- haskell based solution for problem 20.
 import Numeric
+import Data.List
+
+-- http://www.rhinocerus.net/forum/lang-functional/
+-- 95473-converting-number-list-haskell.html#post539294
+numberToList = reverse . unfoldr (\x -> if x == 0 
+                                        then Nothing 
+                                        else let (a,b) = x `quotRem` 10 in Just (b,a))
 
 main :: IO ()
 main = do
-       print $ (product [1..100]::Double)
+       print $ sum $ numberToList (product [1..100])
