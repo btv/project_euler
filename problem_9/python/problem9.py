8e01f38b7352cf0c8e89942d51f771aa76ef0924
Bryce Verdier <bryceverdier@gmail.com>
2010-12-02 15:53:35 -0800

    expoentals are slow in python3

--- problem_9/python/problem9.py
+++ problem_9/python/problem9.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
 python soltion for project euler problem #9.
 """
 
 print( [a*b*(1000 - b -a) for a in range(1,500+1) for b in range(1,500+1) \
-        if a * a + b * b == ((1000 -b -a) ** 2)][0])
+        if a * a + b * b == ((1000 -b -a) * (1000 - b - a))][0])
 
