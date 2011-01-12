a803c9b8cb11b30421e73ca2a22e0110f509cb2f
Bryce Verdier <bryceverdier@gmail.com>
2011-01-11 16:25:22 -0800

    improved algo from website comment. It really works.

--- problem_9/python/problem9.py
+++ problem_9/python/problem9.py
@@ -3,6 +3,6 @@
 python soltion for project euler problem #9.
 """
 
-print( [a*b*(1000 - b -a) for a in range(1,500+1) for b in range(1,500+1) \
+print( [a*b*(1000 - b -a) for a in range(1,500+1) for b in range(a,500+1) \
         if a * a + b * b == ((1000 -b -a) * (1000 - b - a))][0])
 
