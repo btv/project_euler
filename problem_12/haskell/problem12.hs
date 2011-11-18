54d88975c82079e06a9b5a0acc0df59ef1e2b060
Bryce Verdier <bryceverdier@gmail.com>
2011-11-17 19:09:07 -0800

    changed from mod to rem because it's faster. Trying to get the program to run under a minute.

--- problem_12/haskell/problem12.hs
+++ problem_12/haskell/problem12.hs
@@ -1,9 +1,7 @@
 --module Main where
 
-get_div_len number = length $ [1,number] ++  [ y | y <- [2..(number - 1)] , number `mod` y == 0]
+get_div_len number = length $ [1,number] ++  [ y | y <- [2..(number - 1)] , number `rem` y == 0]
 
---triangle num = sum(map digitToInt $ show num)
-
-main :: IO()
-main = do
-  print $ head [ x | x <- [1..], (get_div_len x) >= 500] 
+--main :: IO()
+--main = do
+--  print $ [ get_div_len x | x <- [2,4..]]
