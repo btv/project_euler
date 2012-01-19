729cf5b017ee79fa80b7b7499bba05196b20f50d
Bryce Verdier <bryceverdier@gmail.com>
2012-01-19 08:54:43 -0800

    have problem 12 working in haskell. A little slower than I would like however.

--- problem_12/haskell/problem12.hs
+++ problem_12/haskell/problem12.hs
@@ -1,9 +1,9 @@
 module Main where
 
-get_div_len number = foldr (+) 0 [2 | x <- [1..x], number `mod` x == 0]
-    where x = toInteger . round . sqrt $ fromInteger number
+get_div_len number = foldl1 (+) [2 | x <- [1..x], number `mod` x == 0]
+    where x = round . sqrt $ fromInteger number
 
 main :: IO()
 main = do
-  print $ map (\x -> (get_div_len x ,x)) xs
-  where xs = map (\x -> sum [1..x]) [7..]
+  print . head $ dropWhile (\x -> fst x <= 499) (map (\x -> (get_div_len x ,x)) xs)
+  where xs = map (\y -> sum [1..y]) [7..]
