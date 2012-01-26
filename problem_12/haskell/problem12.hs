5d5074c81c786c7a87838d690b8f2007050ae77a
Bryce Verdier <bryceverdier@gmail.com>
2012-01-25 20:45:55 -0800

    adding State dependencies. Now the hard part.

--- problem_12/haskell/problem12.hs
+++ problem_12/haskell/problem12.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import Control.Monad
+import Control.Monad.State
+
 get_div_len number = foldl1 (+) [2 | x <- [1..x], number `mod` x == 0]
     where x = round . sqrt $ fromInteger number
 
