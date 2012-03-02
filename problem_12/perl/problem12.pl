1bb483a5f48a74e73e86e99acb8e4f3d2fbb7f30
Bryce Verdier <bryceverdier@gmail.com>
2012-03-02 13:28:09 -0800

    added initial perl solution.

---
+++ problem_12/perl/problem12.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $index = 7;
+my $total = 28;
+my $divisors = 0;
+
+sub divisors
+{
+    my ($number) = @_;
+    my $i = 1;
+    my @t;
+
+while( $divisors != 500)
+{
+}
+
+print $total;
