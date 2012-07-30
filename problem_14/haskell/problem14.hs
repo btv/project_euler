ea9851c6a55049b9a3a3dbe5b1b220178c0dd861
Bryce Verdier <bryceverdier@gmail.com>
2012-07-30 09:52:19 -0700

    change main to use maximumBy. Not faster, but cleaner.

--- problem_14/haskell/problem14.hs
+++ problem_14/haskell/problem14.hs
@@ -1,14 +1,15 @@
  module Main where
  import Data.Tuple
- import Data.List (sortBy, iterate)
+ import Data.List (sortBy, iterate, maximumBy, minimumBy)
  import Data.Function (on)
 
  chain' :: Integer -> Int
  chain' n  
      | n < 1 = 0
-     | otherwise = length $ (takeWhile ( > 1) $ iterate (\x -> if even x then x `div` 2 else x * 3 + 1) n) ++ [1]
+     | otherwise = 1 + (length $ (takeWhile ( > 1) $ iterate (\x -> if even x then x `div` 2 else x * 3 + 1) n))
 
  main :: IO ()
  main = do
      let seqx = map (\x -> (x, chain' x)) [999999,999997..3]
-     print . fst . head $ sortBy (flip compare `on` snd) seqx
+     print $ maximumBy (compare `on` snd) seqx
+
