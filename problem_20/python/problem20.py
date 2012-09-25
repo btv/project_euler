fccb8f66e05bc194ac8c808a2fbf27ba2adfd86a
Bryce Verdier <bryceverdier@gmail.com>
2012-09-24 20:39:12 -0700

    removed parenthesis, not needed.

--- problem_20/python/problem20.py
+++ problem_20/python/problem20.py
@@ -6,6 +6,6 @@ solution for problem 20 in python.
 def int_to_list(number):
     return [int(digits) for digits in str(number)]
 
-number = (reduce(lambda x, y: x * y, xrange(1,100+1)))
+number = reduce(lambda x, y: x * y, xrange(1,100+1))
 
 print(sum(int_to_list(number)))
