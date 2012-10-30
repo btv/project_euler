5e5b6f974a787aaaf7b8eed7e77c364d74de886f
Bryce Verdier <bryceverdier@gmail.com>
2012-10-30 14:31:09 -0700

    working solution for problem 21, needs speed improvement though.

--- problem_21/haskell/problem_21.hs
+++ problem_21/haskell/problem_21.hs
@@ -5,3 +5,7 @@ sumOfDivisors = sum . divisors
   --divisors grabbed from 
   -- http://stackoverflow.com/questions/1480563/making-a-list-of-divisors-in-haskell
   where divisors n = [x | x <- [1..(n-1)], n `rem` x == 0]
+
+
+main :: IO ()
+main = print $ sum [ x | x <- [1..9999], x == (sumOfDivisors $ sumOfDivisors x), x /= sumOfDivisors x]
