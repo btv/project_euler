052122e02b5679d88db7662976e2a824d501d54a
Bryce Verdier <bryceverdier@gmail.com>
2012-10-29 20:05:15 -0700

    have correct solution now.

--- problem_17/python/problem17.py
+++ problem_17/python/problem17.py
@@ -1,41 +1,35 @@
 #!/usr/bin/env python3
 
-ones = {1: 'one', 2: 'two', 3: 'three', 4:'four', 5: 'five', 6:'six',
-        7: 'seven', 8: 'eight', 9: 'nine', 0: 'zero'}
+ones = {'1': 'one', '2': 'two', '3': 'three', '4': 'four', '5': 'five',
+        '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine', '0': ''}
 
-tens = {10: 'ten', 20:'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty',
-        60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety'}
+tens = {'2': 'twenty', '3': 'thirty', '4': 'forty', '5': 'fifty',
+        '6': 'sixty', '7': 'seventy', '8': 'eighty', '9': 'ninety'}
 
-teens = {11: 'eleven', 12: 'twelve', 13:'thriteen', 14: 'fourteen',
-         15: 'fifteen', 16: 'sixteen', 17: 'seventeen',
-         18: 'eighteen', 19: 'nineteen'}
+teens = {'10': 'ten', '11': 'eleven', '12': 'twelve', '13': 'thriteen',
+         '14': 'fourteen', '15': 'fifteen', '16': 'sixteen', '17': 'seventeen',
+         '18': 'eighteen', '19': 'nineteen'}
 
-hundreds = {0: 0,
-            1: lambda: len("one hundred and"),
-            2: lambda: len("two hundred and"),
-            3: lambda: len("three hundred and"),
-            4: lambda: len("four hundred and"),
-            5: lambda: len("five hundred and"),
-            6: lambda: len("six hundred and"),
-            7: lambda: len("seven hundred and"),
-            8: lambda: len("eight hundred and"),
-            9: lambda: len("nine hundred and")
+hundreds = {0: 0, 1: "onehundredand", 2: "twohundredand",
+            3: "threehundredand", 4: "fourhundredand",
+            5: "fivehundredand", 6: "sixhundredand",
+            7: "sevenhundredand", 8: "eighthundredand",
+            9: "ninehundredand"
            }
 
 if __name__ == "__main__":
     tot = 0
-    tstr= None
-    for y in range(0,99):
-        t= None
-        try:
-            t,o = tuple(str(y))
-        except ValueError:
-            o = str(y)
-        if t is None:
-            tstr = ones[int(o)]
-        else:
-            tstr = ' '.join(tens[int(t)], ones[int(o)])
-    print(tstr)
-
-    tot += len('one thousand')
+    for h in range(10):
+        for y in range(1,100):
+            try:
+                t,o = tuple(str(y))
+                if t is '1':
+                    tot += len("{h}{t}".format(h=hundreds[h], t=teens[t + o]))
+                else:
+                    tot += len("{h}{t}{o}".format(h = hundreds[h], t=tens[t],
+                                                  o=ones[o]))
+            except ValueError:
+                    tot += len("{h}{o}".format(h= hundreds[h], o=ones[str(y)]))
+                    print("{h}{o}".format(h= hundreds[h], o=ones[str(y)]))
+    tot += len('onethousand')
     print(tot)
