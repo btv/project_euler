fdae9ba19332697360f5dd6ff90915d53d3234e6
Bryce Verdier <bverdier@gmail.com>
2012-11-21 10:55:42 -0800

    modified problem 16 to increase problem and figure out which solution is actually 'faster'.

--- problem_16/haskell/problem16.hs
+++ problem_16/haskell/problem16.hs
@@ -3,4 +3,4 @@ module Main where
 import Data.Char
 
 main :: IO ()
-main = print . sum . map digitToInt . show $ 2 ^ 1000
+main = print . sum . map digitToInt . show $ 2 ^ 100000000
--- problem_16/haskell/problem16_3.hs
+++ problem_16/haskell/problem16_3.hs
@@ -4,4 +4,4 @@ import Data.List (unfoldr)
 import Data.Maybe
 
 main :: IO ()
-main = print . sum . unfoldr (\n -> if n == 0 then Nothing else let (d,r) = divMod n 10 in Just (r,d)) $ 2 ^ 1000
+main = print . sum . unfoldr (\n -> if n == 0 then Nothing else let (d,r) = divMod n 10 in Just (r,d)) $ 2 ^ 100000000
