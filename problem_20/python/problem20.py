99a66384ffa001663241edac426fa8b84c335b09
Bryce Verdier <bverdier@gmail.com>
2013-03-19 21:29:27 -0700

    changed from lambda to mul... it's faster, honest.

--- problem_20/python/problem20.py
+++ problem_20/python/problem20.py
@@ -2,10 +2,12 @@
 """
 solution for problem 20 in python.
 """
+from operator import mul
+
 #taken from problem 8
 def int_to_list(number):
     return [int(digits) for digits in str(number)]
 
-number = reduce(lambda x, y: x * y, xrange(1,100+1))
+number = reduce(mul, xrange(1,101))
 
 print(sum(int_to_list(number)))
