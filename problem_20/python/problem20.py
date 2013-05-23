fd14eb88668bf3127869eab0efe9bcc68f345094
Bryce Verdier <bverdier@gmail.com>
2013-05-22 21:13:03 -0700

    Fixed identity issue.
    This code didn't know if it was python2 or python3... fixed.

--- problem_20/python/problem20.py
+++ problem_20/python/problem20.py
@@ -3,11 +3,12 @@
 solution for problem 20 in python.
 """
 from operator import mul
+from functools import reduce
 
 #taken from problem 8
 def int_to_list(number):
     return [int(digits) for digits in str(number)]
 
-number = reduce(mul, xrange(1,101))
+number = reduce(mul, range(1,101))
 
 print(sum(int_to_list(number)))
