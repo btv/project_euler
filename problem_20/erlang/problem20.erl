7829b8719be75287fa9777b1a78393d4bdf2b4db
Bryce Verdier <bryceverdier@gmail.com>
2013-08-11 16:52:30 +1000

    using sum instead of foldl.

--- problem_20/erlang/problem20.erl
+++ problem_20/erlang/problem20.erl
@@ -1,5 +1,5 @@
 -module(problem20).
--export([get_answer/0]).
+-export([get_answer/0, get_answer2/0]).
 
 product() ->
     %% borrowed from the lists man page
@@ -9,3 +9,7 @@ get_answer() ->
     lists:foldl(fun(X, Sum) -> erlang:list_to_integer([X]) + Sum end,
                 0,
                 erlang:integer_to_list(product())).
+
+get_answer2() ->
+    lists:sum(lists:map(fun(X) -> erlang:list_to_integer([X]) end,
+                        erlang:integer_to_list(product()))).
