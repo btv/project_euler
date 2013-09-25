421fffd18da1b1ed23e5ebe4ca50829db5ae19f9
Bryce Verdier <bryceverdier@gmail.com>
2013-09-25 00:06:52 -0700

    used stdlib factorial instead of hand rolled one. I think it looks cleaner though I'm not sure it runs faster.

--- problem_20/python/problem20.py
+++ problem_20/python/problem20.py
@@ -2,13 +2,12 @@
 """
 solution for problem 20 in python.
 """
-from operator import mul
-from functools import reduce
+from math import factorial
 
 #taken from problem 8
 def int_to_list(number):
     return [int(digits) for digits in str(number)]
 
-number = reduce(mul, range(1,101))
+number = factorial(100)
 
 print(sum(int_to_list(number)))
