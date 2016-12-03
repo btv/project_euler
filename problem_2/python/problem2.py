789ff32b62fc1ef348d48a29609c124cd28af7d0
Bryce Verdier <bryceverdier@gmail.com>
2016-12-02 20:25:55 -0700

    Fixed some syntax mistakes. removed a variabled that wasn't really necessary.

--- problem_2/python/problem2.py
+++ problem_2/python/problem2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-def fibinaci_list( in_list, input1, input2):
+def fibinaci_list(in_list, input1, input2):
   flip = True
-  while ( input1 + input2 < 4000000):
+  while (input1 + input2 < 4000000):
     if( flip == True):
       input1 = input1 + input2
       in_list.append(input1)
@@ -10,7 +10,7 @@ def fibinaci_list( in_list, input1, input2):
     else:
       input2 = input1 + input2
       in_list.append(input2)
-      flip = True 
+      flip = True
 
 def even(n):
   return n % 2 == 0
@@ -19,8 +19,7 @@ def even(n):
 def main():
   first_list = []
   fibinaci_list(first_list, 0, 1)
-  second_list = filter(even, first_list)
-  print "%s" % (sum(second_list))
+  print "%s" % (sum(filter(even, first_list)))
 
 if __name__ == "__main__":
   main()
